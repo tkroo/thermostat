37d5483fa4a797c2474ec14e69b727f527fea660
David Sullivan <treatkor@gmail.com>
2023-02-12 18:53:54 -0800

    update comment about GPIO9 and sdcard mosi

--- common.py
+++ common.py
@@ -3,7 +3,7 @@ from machine import Pin
 # Note for esp32c3
 # mosi and miso are swapped intentionally here because of this issue:
 # https://forum.seeedstudio.com/t/xiao-esp32c3-expansion-board-sd-card-miso-line-stays-low-fix/268404
-# GPIO 9 is strapping pin  on esp32c3 and MOSI doesn't release the pin?
+# GPIO 9 is strapping pin  on esp32c3 and sdcard doesn't release mosi pin?
 # using SoftSPI instead of SPI, avoids this problem. :)
 PINS = {
     "spi_mosi": Pin(9),
